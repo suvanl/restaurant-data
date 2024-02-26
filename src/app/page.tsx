4b445e5a8f8c3d0c2601b2f46220b956b2f02d54
suvanl <32813609+suvanl@users.noreply.github.com>
2024-02-26 16:05:02 +0000

    Add titles to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 export default function HomePage() {
     return (
-        <main className="container relative mx-auto overflow-auto p-4 md:p-16">
+        <main className="container relative mx-auto min-h-dvh items-center space-y-60 overflow-auto p-4 md:p-16">
             <section>
-                <h1 className="text-3xl font-semibold">Restaurant Data</h1>
+                <h1 className="text-lg font-semibold tracking-tight">
+                    Restaurant Data Search
+                </h1>
                 <p>JET Software Engineering Coding Assignment</p>
+            </section>
+
+            <section className="space-y-4">
+                <h2 className="text-balance text-3xl font-medium leading-none tracking-tight">
+                    Find restaurants in a postcode area
+                </h2>
             </section>
         </main>
     );
