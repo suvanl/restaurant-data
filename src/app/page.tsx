d0333baa6d30f31da00c8cc3f96b2cde5a6b9027
suvanl <32813609+suvanl@users.noreply.github.com>
2024-02-26 16:35:00 +0000

    Add search form to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
+
 export default function HomePage() {
     return (
         <main className="container relative mx-auto min-h-dvh items-center space-y-60 overflow-auto p-4 md:p-16">
@@ -12,6 +16,21 @@ export default function HomePage() {
                 <h2 className="text-balance text-3xl font-medium leading-none tracking-tight">
                     Find restaurants in a postcode area
                 </h2>
+
+                <form
+                    action=""
+                    className="flex w-full max-w-2xl items-center gap-x-2"
+                >
+                    <Input
+                        type="search"
+                        name="postcode"
+                        placeholder="Enter a postcode e.g., EC4M 7RF"
+                    />
+                    <Button type="submit" className="flex items-center gap-x-2">
+                        <SearchIcon className="size-5" />
+                        Search
+                    </Button>
+                </form>
             </section>
         </main>
     );
