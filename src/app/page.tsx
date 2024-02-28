18e56e2cd31a7748cce43862d20bc811c6e72204
suvanl <32813609+suvanl@users.noreply.github.com>
2024-02-28 17:10:37 +0000

    Make y-axis padding page-specific
    To avoid excessive space on the results page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { SearchForm } from "@/components/search-form";
 
 export default function HomePage() {
     return (
-        <section className="space-y-4">
+        <section className="space-y-4 py-60 md:py-[16dvh]">
             <h2 className="text-balance text-3xl font-medium leading-none tracking-tight">
                 Find restaurants in a postcode area
             </h2>
